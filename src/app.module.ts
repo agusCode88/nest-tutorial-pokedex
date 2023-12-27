ec6cfd5a8bf7ed53c17c519e4c4dc47b512f715f
Agustin Romero <romero.agustin@seiza-ti.cl>
2023-12-27 09:56:28 -0300

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { join } from 'path'; // Esto ya viene en node
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 
@@ -17,7 +18,9 @@ import { CommonModule } from './common/common.module';
 
     PokemonModule,
 
-    CommonModule
+    CommonModule,
+
+    SeedModule
   ],
 
 })
